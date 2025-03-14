4ab0ff3f3914d08ad2e1eae33351adf3580d180f
CamCoder08 <y30281@gmail.com>
2025-03-14 10:43:48 +0900

    연산자 판별 방식을 switch-case로 변경 및 예외처리 추가 (#2)
    - if-else 문을 switch-case 문으로 변경하여 코드 가독성을 개선
    - 나누기와 나머지 연산에서 0으로 나누는 예외 처리 추가
    - 잘못된 연산자 입력 시 에러 메시지 출력 및 기본값 0 반환 처리
    - Double 타입에서 truncatingRemainder 메서드를 사용하여 나머지 연산 구현

--- Calculator_week1.playground/Contents.swift
+++ Calculator_week1.playground/Contents.swift
@@ -1,24 +1,51 @@
 import UIKit
 
-class Calculator { // 계산기라는 설계도 생성
-    func calculate(op: String, firstNumber: Double, secondNumber: Double) -> Double { // 더욱 세밀한 값을 위해 Double 타입 반환
-        if op == "+" {
-            return firstNumber + secondNumber
-        } else if op == "-" {
-            return firstNumber - secondNumber
-        } else if op == "*" {
-            return firstNumber * secondNumber
-        } else if op == "/" {
-            return firstNumber / secondNumber
-        } else {
-            return 0 // 잘못된 연산자가 입력될 경우 기본적으로 0을 반환
+class Calculator {
+    func calculate(op: String, firstNumber: Double, secondNumber: Double) -> Double {
+        switch op { // if-else 대신 연산자 비교를 switch-case 구문으로 변경
+        case "+":
+            let result = firstNumber + secondNumber
+            print(result)
+            return result
+        case "-":
+            let result = firstNumber - secondNumber
+            print(result)
+            return result
+        case "*":
+            let result = firstNumber * secondNumber
+            print(result)
+            return result
+        case "/":
+            if secondNumber == 0 { // 두 번째 숫자가 0인 경우 나누기 예외 처리
+                print("Error: 0으로 나눌 수 없습니다")
+                return 0
+            }
+            let result = firstNumber / secondNumber // 정상적 나누기 연산
+            print(result)
+            return result
+        case "%":
+            if secondNumber == 0 { // 나머지 연산 시 0 나누기 예외 처리
+                print("Error: 나머지 연산에서 0으로 나눌 수 없습니다.")
+                return 0 // 기본값 0 반환
+            }
+            let result = firstNumber.truncatingRemainder(dividingBy: secondNumber)
+            print(result)
+            return result
+        default:
+            print("Error: 잘못된 연산자입니다.")
+            return 0
         }
     }
 }
-// Calculator 클래스의 인스턴스를 생성
+// Calculator 클래스의 인스턴스 생성
 let calculator = Calculator()
 
 calculator.calculate(op: "+", firstNumber: 2, secondNumber: 5)
 calculator.calculate(op: "-", firstNumber: 10, secondNumber: 5)
 calculator.calculate(op: "*", firstNumber: 3, secondNumber: 6)
-calculator.calculate(op: "/", firstNumber: 8, secondNumber: 2)
+calculator.calculate(op: "/", firstNumber: 8, secondNumber: 0) // 0 나누기 예외 처리 확인
+calculator.calculate(op: "%", firstNumber: 4, secondNumber: 0) // 0 나머지 연산 예외 처리 확인
+calculator.calculate(op: "ㅁ", firstNumber: 4, secondNumber: 2) // 잘못된 연산자 예외 처리 확인
+
+
+
